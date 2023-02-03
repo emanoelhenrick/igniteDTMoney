f127f820b66e87e9cdb455a0b38131cc7b163879
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-02-03 19:33:16 -0300

    add search to json api

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { MagnifyingGlass} from 'phosphor-react'
 import { useForm } from "react-hook-form";
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string()
@@ -12,6 +14,8 @@ type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
 
+  const { fetchTransactions } = useContext(TransactionsContext)
+
   const {
     register,
     handleSubmit,
@@ -20,8 +24,8 @@ export function SearchForm() {
     resolver: zodResolver(searchFormSchema)
   })
 
-  function handleSearchTransactions(data: SearchFormInputs) {
-
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
   }
 
 
